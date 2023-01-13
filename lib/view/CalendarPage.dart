d7650f58a843b302238428d7637f16f1e2939d17
miki0412 <chanmiki08412@gmail.com>
2023-01-13 15:27:52 +0900

    曜日、日付への色付け

--- lib/view/CalendarPage.dart
+++ lib/view/CalendarPage.dart
@@ -8,6 +8,17 @@ class CalendarPage extends ConsumerWidget{
   DateTime Datetime = DateTime.now();
   DateTime FirstDay = DateTime.utc(2022,1,1);
   DateTime LastDay = DateTime.utc(2023,12,31);
+  Color _textColor(DateTime day) {
+    const _defaultTextColor = Colors.black;
+
+    if(day.weekday == DateTime.sunday){
+      return Colors.red;
+    }
+    if(day.weekday == DateTime.saturday){
+      return Colors.blue;
+    }
+    return _defaultTextColor;
+  }
 
   @override
   Widget build(BuildContext context,WidgetRef ref){
@@ -27,7 +38,30 @@ class CalendarPage extends ConsumerWidget{
           focusedDay: Datetime,
           locale: 'ja_JP',
           calendarBuilders: CalendarBuilders(
-
+            dowBuilder: (_,day) {//dowBuilder:曜日のセル(月、火、水、木、金、土、日）に対して編集をする
+              if (day.weekday == DateTime.sunday){
+              return const Center(
+                child: Text("日",
+                  style: TextStyle(color:Colors.red),
+                ),
+              );
+            }
+              if(day.weekday == DateTime.saturday){
+                return const Center(
+                  child:Text("土",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                );
+              }
+              return null;},
+            defaultBuilder: (BuildContext context,DateTime day,DateTime focusedDay){//defaultBuilder:日にちのセルに対して編集する
+              return Text(
+                  day.day.toString(),
+                  style: TextStyle(
+                    color: _textColor(day),
+                  ),
+              );
+            }
           ),
         )
       ),
