7a90268a759b444c2d74e010fc4c284b82df7a44
miki0412 <chanmiki08412@gmail.com>
2023-01-26 22:20:22 +0900

    追加

--- lib/view/CalendarPage.dart
+++ lib/view/CalendarPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:calendar/view/color_model.dart';
 
 class CalendarPage extends ConsumerWidget{
   CalendarPage({super.key});
@@ -8,16 +10,15 @@ class CalendarPage extends ConsumerWidget{
   DateTime Datetime = DateTime.now();
   DateTime FirstDay = DateTime.utc(2022,1,1);
   DateTime LastDay = DateTime.utc(2023,12,31);
-  Color _textColor(DateTime day) {
-    const _defaultTextColor = Colors.black;
 
+  Color _textColor(DateTime day) {
     if(day.weekday == DateTime.sunday){
-      return Colors.red;
+      return ColorModel.red;
     }
     if(day.weekday == DateTime.saturday){
-      return Colors.blue;
+      return ColorModel.blue;
     }
-    return _defaultTextColor;
+    return ColorModel.primary;
   }
 
   @override
@@ -25,35 +26,38 @@ class CalendarPage extends ConsumerWidget{
     return Scaffold(
       appBar: AppBar(
         title: const Text('カレンダー'),
-        backgroundColor: Colors.blue,
       ),
       body:Center(
         child:TableCalendar<dynamic>(
           headerStyle: const HeaderStyle(
-            formatButtonVisible: true,
+            formatButtonVisible: false,
             titleCentered: true,
           ),
           firstDay: FirstDay,
           lastDay: LastDay,
           focusedDay: Datetime,
-          locale: 'ja_JP',
+          locale: 'ja',
           calendarBuilders: CalendarBuilders(
             dowBuilder: (_,day) {//dowBuilder:曜日のセル(月、火、水、木、金、土、日）に対して編集をする
-              if (day.weekday == DateTime.sunday){
-              return const Center(
-                child: Text("日",
-                  style: TextStyle(color:Colors.red),
-                ),
-              );
-            }
-              if(day.weekday == DateTime.saturday){
-                return const Center(
-                  child:Text("土",
-                    style: TextStyle(color: Colors.blue),
-                  ),
-                );
-              }
-              return null;},
+              final text = DateFormat.E('ja').format(day);
+                if(day.weekday == DateTime.sunday){
+                  return Center(
+                      child:Text(
+                        text,
+                        style: const TextStyle(color:ColorModel.red),
+                      )
+                  );
+                }
+                if(day.weekday == DateTime.saturday){
+                  return Center(
+                    child:Text(
+                      text,
+                      style: const TextStyle(color: ColorModel.blue),
+                    )
+                  );
+                }
+                return null;
+            },
             defaultBuilder: (BuildContext context,DateTime day,DateTime focusedDay){//defaultBuilder:日にちのセルに対して編集する
               return Text(
                   day.day.toString(),
---
+++ lib/view/color_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorModel {
+  static const primary = Color(0xFF000000);
+  static const red = Color(0xFFFF0000);
+  static const blue = Color(0xFF0000FF);
+  static const lightgray = Color(0xFFd3d3d3);
+}
+
+Color BorderColor = Color(0xFF000000);
